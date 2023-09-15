32dbe94da0dd4b2e9917897939ec94f511923ef0
Melanieleonsoza <melanie.leon2016@uteq.edu.ec>
2023-09-14 23:26:32 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mapsgoogle/MainActivityImpl.java
@@ -0,0 +1,8 @@
+package com.example.mapsgoogle;
+
+public class MainActivityImpl extends MainActivity {
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+}
