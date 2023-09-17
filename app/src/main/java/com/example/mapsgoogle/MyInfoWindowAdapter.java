32177fd7bd938b3e322dd89d227b1b3baa9ca57f
Melanieleonsoza <135043102+Melanieleonsoza@users.noreply.github.com>
2023-09-17 10:21:19 -0500

    Update

---
+++ app/src/main/java/com/example/mapsgoogle/MyInfoWindowAdapter.java
@@ -0,0 +1,32 @@
+package com.example.mapsgoogle;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+public class MyInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+    private final View mWindow;
+
+    public MyInfoWindowAdapter(Context context) {
+        mWindow = LayoutInflater.from(context).inflate(R.layout.ventanainformacion, null);
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        TextView title = mWindow.findViewById(R.id.title3);
+        TextView snippet = mWindow.findViewById(R.id.snippet4);
+        title.setText(marker.getTitle());
+        snippet.setText(marker.getSnippet());
+        return mWindow;
+    }
+
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
+}
